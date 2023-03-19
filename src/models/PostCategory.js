00c51955732a405f70c9ff86bbfb3731df94cc5d
DouglasDainese <douglas_dainese@hotmail.com>
2023-03-19 00:28:35 -0300

    Feat: cria model PostCategory, resolução requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import {'sequelize}.DataTypes } DataTypes
+ **/ 
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', 
+  {
+    postId: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+    },
+    categoryId: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+    }
+  },
+  {
+    timestamps: false,
+    tableName: 'post_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPost',
+      through: PostCategory,
+      foreignKey: 'categoryId', 
+      otherKey: 'post_Id', 
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Category',
+      through: PostCategory,
+      foreignKey: 'postId', 
+      otherKey: 'categoryId', 
+    });
+  };
+
+  return PostCategory
+};
